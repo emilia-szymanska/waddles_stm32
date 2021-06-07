36b71c934584c20cbd8010ed94c1cb3b1ed8a57a
emilia-szymanska <emiliaszym@gmail.com>
2021-06-07 09:14:15 +0200

    Expand motors module with specific control for driving

--- Core/Inc/motors.h
+++ Core/Inc/motors.h
@@ -4,8 +4,42 @@
 #include "tim.h"
 #include "gpio.h"
 
+#define FORWARD 1
+#define BACKWARD 0
+#define SPEED_CONST 50
+#define SPEED_STOP 0
+
 void MOTORS_start();
 
+void MOTORS_stop();
+
 void MOTORS_set_speed(uint8_t motor_nr, uint8_t speed);
+
+void MOTORS_set_polarity(uint8_t motor_nr, uint8_t polarity);
+
+void MOTORS_go_forward();
+void MOTORS_go_backward();
+void MOTORS_go_left();
+void MOTORS_go_right();
+void MOTORS_rotate();
+void MOTORS_go_forwardleft();
+void MOTORS_go_forwardright();
+void MOTORS_go_backwardright();
+void MOTORS_go_backwardleft();
+
+void MOTORS_M1_forward();
+void MOTORS_M2_forward();
+void MOTORS_M3_forward();
+void MOTORS_M4_forward();
+
+void MOTORS_M1_backward();
+void MOTORS_M2_backward();
+void MOTORS_M3_backward();
+void MOTORS_M4_backward();
+
+void setpoints_to_zeros(uint16_t *setpoints_array);
+void setpoints_to_const(uint16_t *setpoints_array);
+void setpoints_to_fr_bl(uint16_t *setpoints_array);
+void setpoints_to_fl_br(uint16_t *setpoints_array);
 
 #endif /* INC_MOTORS_H_ */
--- Core/Src/motors.c
+++ Core/Src/motors.c
@@ -18,6 +18,14 @@ void MOTORS_start()
 	HAL_GPIO_WritePin(M4_PHASE_GPIO_Port, M4_PHASE_Pin, SET);
 }
 
+void MOTORS_stop()
+{
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_3);
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_4);
+}
+
 
 void MOTORS_set_speed(uint8_t motor_nr, uint8_t speed)
 {
@@ -41,7 +49,7 @@ void MOTORS_set_speed(uint8_t motor_nr, uint8_t speed)
 
 void MOTORS_set_polarity(uint8_t motor_nr, uint8_t polarity)
 {
-	if(polarity != 0 && polarity != 1) return;
+	if(polarity != FORWARD && polarity != BACKWARD) return;
 	switch(motor_nr)
 	{
 		case 1: HAL_GPIO_WritePin(M1_PHASE_GPIO_Port, M1_PHASE_Pin, polarity);
@@ -56,4 +64,140 @@ void MOTORS_set_polarity(uint8_t motor_nr, uint8_t polarity)
 		case 4: HAL_GPIO_WritePin(M4_PHASE_GPIO_Port, M4_PHASE_Pin, polarity);
 				return;
 	}
+}
+
+void MOTORS_M1_forward()
+{
+	MOTORS_set_polarity(1, FORWARD);
+}
+
+void MOTORS_M2_forward()
+{
+	MOTORS_set_polarity(2, BACKWARD);
+}
+
+void MOTORS_M3_forward()
+{
+	MOTORS_set_polarity(3, FORWARD);
+}
+
+void MOTORS_M4_forward()
+{
+	MOTORS_set_polarity(4, BACKWARD);
+}
+
+void MOTORS_M1_backward()
+{
+	MOTORS_set_polarity(1, BACKWARD);
+}
+
+void MOTORS_M2_backward()
+{
+	MOTORS_set_polarity(2, FORWARD);
+}
+
+void MOTORS_M3_backward()
+{
+	MOTORS_set_polarity(3, BACKWARD);
+}
+
+void MOTORS_M4_backward()
+{
+	MOTORS_set_polarity(4, FORWARD);
+}
+
+void MOTORS_go_forward()
+{
+	MOTORS_M1_forward();
+	MOTORS_M2_forward();
+	MOTORS_M3_forward();
+	MOTORS_M4_forward();
+}
+
+void MOTORS_go_backward()
+{
+	MOTORS_M1_backward();
+	MOTORS_M2_backward();
+	MOTORS_M3_backward();
+	MOTORS_M4_backward();
+}
+
+void MOTORS_go_left()
+{
+	MOTORS_M1_backward();
+	MOTORS_M2_forward();
+	MOTORS_M3_backward();
+	MOTORS_M4_forward();
+}
+
+void MOTORS_go_right()
+{
+	MOTORS_M1_forward();
+	MOTORS_M2_backward();
+	MOTORS_M3_forward();
+	MOTORS_M4_backward();
+}
+
+void MOTORS_rotate()
+{
+	MOTORS_M1_backward();
+	MOTORS_M2_backward();
+	MOTORS_M3_forward();
+	MOTORS_M4_forward();
+}
+
+void MOTORS_go_forwardleft()
+{
+	MOTORS_M2_forward();
+	MOTORS_M4_forward();
+}
+
+void MOTORS_go_forwardright()
+{
+	MOTORS_M1_forward();
+	MOTORS_M3_forward();
+}
+
+void MOTORS_go_backwardright()
+{
+	MOTORS_M2_backward();
+	MOTORS_M4_backward();
+}
+
+void MOTORS_go_backwardleft()
+{
+	MOTORS_M1_backward();
+	MOTORS_M3_backward();
+}
+
+void setpoints_to_zeros(uint16_t *setpoints_array)
+{
+	setpoints_array[0] = SPEED_STOP;
+	setpoints_array[1] = SPEED_STOP;
+	setpoints_array[2] = SPEED_STOP;
+	setpoints_array[3] = SPEED_STOP;
+}
+
+void setpoints_to_const(uint16_t *setpoints_array)
+{
+	setpoints_array[0] = SPEED_CONST;
+	setpoints_array[1] = SPEED_CONST;
+	setpoints_array[2] = SPEED_CONST;
+	setpoints_array[3] = SPEED_CONST;
+}
+
+void setpoints_to_fr_bl(uint16_t *setpoints_array)
+{
+	setpoints_array[0] = SPEED_CONST;
+	setpoints_array[1] = SPEED_STOP;
+	setpoints_array[2] = SPEED_CONST;
+	setpoints_array[3] = SPEED_STOP;
+}
+
+void setpoints_to_fl_br(uint16_t *setpoints_array)
+{
+	setpoints_array[0] = SPEED_STOP;
+	setpoints_array[1] = SPEED_CONST;
+	setpoints_array[2] = SPEED_STOP;
+	setpoints_array[3] = SPEED_CONST;
 }
